0a8829a9c2a4e2ea1d03ee1bc7424a104c695db1
Vincent Chong <ws.chong.sg@gmail.com>
2022-06-05 15:59:06 +0800

    vs2

--- colab-github.sh
+++ colab-github.sh
@@ -18,7 +18,7 @@ echo "----------------------"
 # echo "----------------------"
 
 git_token=$1
-echo "git_token $git_token"
+echo "git_token ${git_token}"
 username=VincentChong123
 repository=00_sutd_modeling
 echo "$git_token"
