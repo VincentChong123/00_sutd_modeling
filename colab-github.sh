cc7aa9b0a17724870d6c0fe120b5e9b8ba3da1c9
Vincent Chong <ws.chong.sg@gmail.com>
2022-06-05 14:46:11 +0800

    vs2

--- colab-github.sh
+++ colab-github.sh
@@ -11,27 +11,26 @@
 
 # git init
 git add .
-git commit -m "1st colab"
+git commit -m "vs2"
 echo "----------------------"
-git remote -v
-git remote remove origin
-echo "----------------------"
+# git remote -v
+# git remote remove origin
+# echo "----------------------"
 
-
-git_token=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
-username=VincentChong123
-repository=00_sutd_modeling
-echo "$git_token"
-# echo ${git_token}
-git remote add origin "https://${git_token}@github.com/${username}/${repository}.git"
-echo "----------------------"
+# git_token=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
+# username=VincentChong123
+# repository=00_sutd_modeling
+# echo "$git_token"
+# # echo ${git_token}
+# git remote add origin "https://${git_token}@github.com/${username}/${repository}.git"
+# echo "----------------------"
 #!git remote add origin https://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@github.com/{username}/{repository}.git
 git remote -v
-echo "----------------------"
-# git branch
-# git branch -r
-echo "2----------------------"
-git branch -M main
+# echo "----------------------"
+# # git branch
+# # git branch -r
+# echo "2----------------------"
+# git branch -M main
 echo "1----------------------"
 # git branch
 # git branch -r
